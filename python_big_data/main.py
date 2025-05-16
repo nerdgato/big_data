07eb396c55525deb2b9971a9b56fbd3adb2377cf
unknown <PC17@MAAWSA02LC0917.duoc.acad>
2025-05-16 15:43:51 -0400

    Todo a csv

--- python_big_data/main.py
+++ python_big_data/main.py
@@ -21,11 +21,18 @@ def get_data():
         provincia_query = 'SELECT * FROM PROVINCIA'
         region_query = 'SELECT * FROM REGION'
         sucursal_query = 'SELECT * FROM SUCURSAL'
+        articulos_query = 'SELECT * FROM ARTICULOS'
+        canal_query = 'SELECT * FROM CANAL'
+        detalle_venta_query = 'SELECT * FROM DETALLE_VENTA'
+        vendedores_query = 'SELECT * FROM VENDEDORES'
+        ventas_query = 'SELECT * FROM VENTAS'
+        
 
         df_comuna = pd.read_sql(comuna_query, cnx)
         df_provincia = pd.read_sql(provincia_query, cnx)
         df_region = pd.read_sql(region_query, cnx)
         df_sucursal = pd.read_sql(sucursal_query, cnx)
+        
 
         # hacer JOIN para unir todo en una tabla ubicaciones
         df = df_sucursal.merge(df_comuna, on='COMUNA_ID', how='left') \
@@ -41,14 +48,47 @@ def get_data():
 
         print('\nDatos finales de UBICACIONES (columnas limpias y renombradas)')
         print(df_final.head(10))
+        
+        # ARTICULOS
+        df_articulos = pd.read_sql(articulos_query, cnx)
+        
+        # CANAL
+        df_canal = pd.read_sql(canal_query, cnx)
+        
+        # DETALLE_VENTA
+        df_detalle_venta = pd.read_sql(detalle_venta_query, cnx)
+        
+        # VENDEDORES
+        df_vendedores = pd.read_sql(vendedores_query, cnx)
+        # VENTAS
+        df_ventas = pd.read_sql(ventas_query, cnx)
+        
+        
 
-        # guardar excel
-        archivo_excel = 'Americans.xlsx'
-        with pd.ExcelWriter(archivo_excel) as writer:
-            df_clientes.to_excel(writer, sheet_name='Clientes', index=False)
-            df_final.to_excel(writer, sheet_name='Ubicaciones', index=False)
+        # guardar ubicaciones en CSV
+        df_final.to_csv('ubicaciones.csv', index=False, sep=';', encoding='utf-8-sig' )
+        print('\nSe guardaron los datos de ubicaciones en ubicaciones.csv')
+        # guardar clientes en CSV
+        df_clientes.to_csv('clientes.csv', index=False, sep=';', encoding='utf-8-sig' )
+        print('\nSe guardaron los datos de clientes en clientes.csv')
+        # guardar articulos en CSV
+        df_articulos.to_csv('articulos.csv', index=False, sep=';', encoding='utf-8-sig' )
+        print('\nSe guardaron los datos de articulos en articulos.csv')
+        # guardar canal en CSV
+        df_canal.to_csv('canal.csv', index=False, sep=';', encoding='utf-8-sig' )
+        print('\nSe guardaron los datos de canal en canal.csv')
+        # guardar detalle_venta en CSV
+        df_detalle_venta.to_csv('detalle_venta.csv', index=False, sep=';', encoding='utf-8-sig' )
+        print('\nSe guardaron los datos de detalle_venta en detalle_venta.csv')
+        # guardar vendedores en CSV
+        df_vendedores.to_csv('vendedores.csv', index=False, sep=';', encoding='utf-8-sig' )
+        print('\nSe guardaron los datos de vendedores en vendedores.csv')
+        # guardar ventas en CSV
+        df_ventas.to_csv('ventas.csv', index=False, sep=';', encoding='utf-8-sig' )
+        print('\nSe guardaron los datos de ventas en ventas.csv')
+        
+        
 
-        print(f'\nSe almacenaron los datos en {archivo_excel}')
 
     except Exception as e:
         print("Algo salió mal\n\n", e)
